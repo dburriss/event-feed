619cfb4a54fe97be223fd5806b0790c7a1386d80
Devon Burriss <devon@chimplab.co>
2022-03-14 19:48:06 +0100

    Renames testing

---
+++ test/building/EventFeed.Tests.Unit/LibraryTests.fs
@@ -0,0 +1,33 @@
+namespace EventFeed.Tests
+
+module LibraryTests =
+
+    open Xunit
+    open EventFeed
+    open System
+    open System.Text.Json
+
+    [<Fact>]
+    let ``NewFeedEvent with default has Id and name`` () =
+        let name = "test-event"
+        let ev = FeedEvent.withDefaults name """{ "test" : true }"""
+        Assert.NotEqual(Guid.Empty, ev.EventId)
+        Assert.Equal(name, ev.EventName)
+
+    [<Fact>]
+    let ``NewFeedEvent sets activity trace and span`` () =
+        let ev = FeedEvent.withDefaults "test-event" """{ "test" : true }"""
+        Assert.NotEmpty(ev.TraceId)
+        Assert.NotEmpty(ev.SpanId)
+
+    [<Fact>]
+    let ``NewFeedEvent sets date and time`` () =
+        let ev = FeedEvent.withDefaults "test-event" """{ "test" : true }"""
+        Assert.Equal(DateTimeOffset.UtcNow.Date, ev.CreatedAt.Date)
+
+    [<Fact>]
+    let ``NewFeedEvent with object payload is serialized`` () =
+        let anObject = {| AValue = "value" |}
+        let ev = FeedEvent.fromObj "test-event" anObject None None None None
+        let deserialized = JsonSerializer.Deserialize<{| AValue: string |}>(ev.Payload)
+        Assert.Equal(anObject.AValue, deserialized.AValue)
